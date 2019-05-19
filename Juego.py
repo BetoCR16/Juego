c65c65fbba9686f6b5638a5fb89aae90b918ebee
Roberto Carlos Méndez López <rcarlosml16@gmail.com>
2019-05-19 00:18:50 -0600

    Tercer commit - Ventanas se ocultan

--- Juego.py
+++ Juego.py
@@ -17,37 +17,49 @@ while play == True:
         pc = random.choice(options)
         if varOption.get()==1:
             if pc == '1':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Ambos sacamos papel. EMPATE")
                 
             elif pc == '3':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Yo saque tijeras. PIERDES")
                 
             elif pc == '2':
+                raiz.withdraw()
                 messagebox.showinfo("Eleccion", "Yo saque piedra. Papel le gana a piedra. GANASTE")
         
         elif varOption.get()==2:
             if pc == '1':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Saque papel. PIERDES")
                 
             elif pc == '3':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Ambos sacamos piedra. EMPATE")
                 
             elif pc == '2':
+                raiz.withdraw()
                 messagebox.showinfo("Resultado", "Yo saque tijeras. GANASTE")
 
         elif varOption.get()==3:
             if pc == '1':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Saque papel. GANASTE")
-                
+                           
             elif pc == '3':
+                raiz.withdraw()
                 messagebox.showinfo('Resultado',"Ambos sacamos tijera. EMPATE")
                 
+                
             elif pc == '2':
+                raiz.withdraw()
                 messagebox.showinfo("Resultado", "Yo saque piedra. PIERDES")
-            
+                
+          
         valor=messagebox.askquestion("Otra vez", "¿Quieres volver a jugar?")
         if valor == "yes": 
             play = True
+            raiz.deiconify()
         else:
             raiz.destroy()
             
